9e0e57f8881d12afd013fccbd3956f9ae40b24b0
adzict <adzic.tanja@gmail.com>
2024-04-19 12:02:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ def display_profile(profile):
         st.write(f"- {key}: {value}")
 
 def main():
-    st.title("Explore Student Data")
+    st.title("TA Center Student Database")
 
     # Load data
     data_load_state = st.text("Loading data...")
